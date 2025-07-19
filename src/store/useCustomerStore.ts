0924bb3c89c4eb67107e37b8054126f75b11fcd6
NMKenuka <kenukaruna08@gmail.com>
2025-07-19 14:40:49 +0530

    Sending Order Details to the backend.

--- src/store/useCustomerStore.ts
+++ src/store/useCustomerStore.ts
@@ -7,12 +7,18 @@ interface CustomerInfo {
 
 interface CustomerStore {
   customerInfo: CustomerInfo | null;
+  discountCode: string;
   setCustomerInfo: (info: CustomerInfo) => void;
   clearCustomerInfo: () => void;
+  setDiscountCode: (code: string) => void;
+  clearDiscountCode: () => void;
 }
 
 export const useCustomerStore = create<CustomerStore>((set) => ({
   customerInfo: null,
+  discountCode: "",
   setCustomerInfo: (info) => set({ customerInfo: info }),
   clearCustomerInfo: () => set({ customerInfo: null }),
+  setDiscountCode: (code) => set({ discountCode: code }),
+  clearDiscountCode: () => set({ discountCode: "" }),
 }));
