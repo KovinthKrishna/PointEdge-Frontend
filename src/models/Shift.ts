a47474af67193166b60cafe6a21e9f797d453669
Thinura Senitha Kahaduwa <thinurakahaduwa@gmail.com>
2025-07-21 14:32:02 +0530

    shift models file updated

--- src/models/Shift.ts
+++ src/models/Shift.ts
@@ -1,9 +1,21 @@
 export interface ShiftData {
+  employeeId: number;
+  employeeName: string;
+  role: string;
+  shiftDate: string;
+  clockIn: string;
+  clockOut: string;
+  otHours: string;
+  workingHours: string;
   shiftType: string;
+  totalOrders: number;
+  totalSales: number;
+  totalWorkingHours: string;
+  
+  // Computed fields for display
   startTime: string;
   endTime: string;
   break: string;
-  otHours: string;
   location: string;
   totalHours: string;
   orders: string;
